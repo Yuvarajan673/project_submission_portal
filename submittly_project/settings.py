fccb98e1a54a5031e47e32eb1c0faf6268aaa63a
Yuvarajan <syuvarajan353@gmail.com>
2025-12-24 13:32:56 +0530

    Changed ALLOWED_HOST to *

--- submittly_project/settings.py
+++ submittly_project/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv("DEBUG")
 
-ALLOWED_HOSTS = [".fly.dev"]
+ALLOWED_HOSTS = ["*"]
 
 
 AUTH_USER_MODEL = 'submittly.User'
